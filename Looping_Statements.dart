a212031c44f1ede575a03cb52f79b85ed25b878b
HarshThanki7777 <141205818+HarshThankix13@users.noreply.github.com>
2023-08-02 10:57:13 +0530

    push

---
+++ Looping_Statements.dart
@@ -0,0 +1,47 @@
+/* 
+
+There Are Mainly 3 Types Of loops 
+
+1) For Loop
+    -> For Each 
+2) While Loop 
+3) Do... While Loop
+
+For Loop : For Loop Is a Sequence Control Loop 
+
+*/
+
+// For Loop 
+
+void main() {
+    for ( int i = 1; i <= 5 ; i++){
+        print("$i");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// For_Each Loop
+
+/*
+
+For Each Loop Iterates Over All Elements In Some Container And Pass The Element To Specific Function
+
+Syntax :
+  Collection.foreach(void f(value))
+
+
+ */

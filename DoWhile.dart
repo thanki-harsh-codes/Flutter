d57483e5dba7b18cfe4f9ac59b31a2945d97d35b
HarshThanki7777 <141205818+HarshThankix13@users.noreply.github.com>
2023-08-04 09:09:20 +0530

    push

---
+++ DoWhile.dart
@@ -0,0 +1,18 @@
+/* 
+
+DO While Loop 
+
+-> Exit Control Loop
+-> In Condition Check At Exit Lvl There Are Possibilities Of At Least 1 Time Loop Execution
+
+
+*/
+
+void main(){
+  int i = 6;
+  do{
+    print(i);
+    i++;
+
+  }while (i <= 5);
+}
---
+++ List.dart
@@ -0,0 +1,12 @@
+void main() {
+  List Shopping_List = ["Fruits" , "Vegetable" , "Bread ", "MilkBottle"];
+
+  print(Shopping_List);
+  // ignore: unused_local_variable
+  int count  = 0;
+  for ( var item in Shopping_List)
+  {
+    print(item);
+    count++;
+  }
+}
